8e0e56f4bb308e79082171a2e74b8e9441ef6bc7
maxwell <techgbenga@gmail.com>
2024-06-29 22:51:17 +0100

    chore: Update route paths for paystack webhook and payment callback

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,10 @@ Route::post('/verify-payment', 'App\Http\Controllers\UserController@verifyPaymen
 
 
  //webhook
- Route::post('paystack/webhook', 'App\Http\Controllers\UserController@paystack_webhook')->name('paystack_webhook');
+ Route::post('/paystack/webhook', 'App\Http\Controllers\UserController@paystack_webhook')->name('paystack_webhook');
 
  //call back
- Route::get('payment/callback', 'App\Http\Controllers\UserController@paystack_callback')->name('paystack_callback');
+ Route::get('/payment/callback', 'App\Http\Controllers\UserController@paystack_callback')->name('paystack_callback');
 
  //initialize payment
  Route::post('initialize-payment', 'App\Http\Controllers\UserController@initialize_payment')->name('pay');
